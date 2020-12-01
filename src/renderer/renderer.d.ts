8959ecfc925359b463b66f9b6307aa6eb6468bce
CoolSnow <coolsnow2020@gmail.com>
2020-12-01 18:19:40 +0800

    update

--- src/renderer/renderer.d.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-unused-vars */
-
-declare module '*.vue' {
-  import { Component } from 'vue'
-  const component: Component
-  export default component
-}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './index.html',
+      './src/**/*.vue',
+      './src/**/*.js',
+      './src/**/*.ts',
+    ],
+  },
+  theme: {
+    extend: {
+      opacity: {
+        10: '0.1',
+      },
+    },
+  },
+}
